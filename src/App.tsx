0c75ddcc34f04037beec42703e962084d0e20994
Milena <milenasvitras@gmail.com>
2025-12-02 10:05:46 -0300

    :sparkles: Criação das rotas no componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Footer from "./footer/Footer"
 import Navbar from "./navbar/Navbar"
 import Home from "./pages/home/Home"
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
-  );
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        
+        <Navbar />
+        
+        <div className="flex-1 w-full pt-16 bg-slate-200">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
+
